862b7245c6ffd6e2f155e01bfe4f621e66fa3a39
TheJadeDrag0n <47540316+TheJadeDrag0n@users.noreply.github.com>
2019-05-17 12:40:22 +1200

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,11 +1,40 @@
 from bottle import run, route, view, get, post, request, static_file
+from itertools import count
 
 
 
-#==[ Pages ]==#
+class Questions:
+    
+    _ids = count(0)
+    
+    
+    def __init__(self, image, question, choice_1, choice_2, choice_3, choice_4):
+        
+        self.id = next(self._ids)
+        self.cover = image
+        self.question = question
+        self.choice_1 = choice_1
+        self.choice_2 = choice_2
+        self.choice_3 = choice_3
+        self.choice_4 = choice_4        
 
 
-#--[ Index ]--#
+#==[ Test Data ]==#
+
+QuestionList = [
+    
+    Questions("logo.png", "test_question", "test_question_1", "test_question_2", "test_question_3", "test_question_4"),
+    Questions("logo.png", "test_question", "test_question_1", "test_question_2", "test_question_3", "test_question_4"),
+    Questions("logo.png", "test_question", "test_question_1", "test_question_2", "test_question_3", "test_question_4")
+    
+    ]
+
+
+
+
+#===[ Pages ]===#
+
+ #--[ Index ]--#
 
 @route("/")
 @view("index")
@@ -14,20 +43,22 @@ def index():
     pass
 
 
-#--[ Quiz ]--#
+ #--[ Quiz ]--#
+ 
 @route("/quiz")
 @view("quiz")
 def quiz():
     
-    pass
+    data = dict (question_list=QuestionList)
+    return data     
 
 
+#===[ Images ]===#
 
-#==[ Images ]==#
 @route('/pictures/<filename>')
 def picture(filename):
     return static_file(filename, root='./images')
 
 
-
+#Run Server
 run(host='0.0.0.0',port = 8080, reloader=True, debug=True)
